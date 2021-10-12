8e20ad3bd7c87b68bcc743db539c5007f7488659
Hermassi Nadir <69248484+Hernado998@users.noreply.github.com>
2021-10-12 23:13:31 +0100

    Update mqttfunc.py

--- mqttfunc.py
+++ mqttfunc.py
@@ -2,12 +2,12 @@ from paho.mqtt import client as mqtt_client
 import random as rnd
 from InitiateCom import InitiateCom
 
-broker = '10.42.0.219'
-port = 1883
+broker = "10.42.0.219"
+port = "1883"
 topic = "test"
-client_idx = 0
+client_idx = "0"
 clientID ="0"
-msgID = 5000
+msgID = "5000"
 username = "admin"
 password = "admin"
 
@@ -23,37 +23,42 @@ class QuectelEG95Mqtt:
         self.initiate.connectQuectelEG95()
 
     def openNetwork_mqtt(self):
-        command = 'AT+QMTOPEN='+str(self.clientidx)+','+self.broker+','+str(self.port)+'\r'
-        self.initiate.sendATCommand(command)
+        args= self.clientidx+","+self.broker+","+self.port+"\r\n"
+        Response = self.sendATCommand(OPEN_NETWORK_MQTT,args)
+        return Response
     
     def closeNetwork_mqtt(self):
-        command = 'AT+QMTCLOSE='+str(self.clientidx)+'\r'
-        self.initiate.sendATCommand(command)
+        args = self.clientidx+"\r\n"
+        Response = self.sendATCommand(CLOSE_NETWORK_MQTT,args)
+        return Response
     
     def connect_mqtt(self,clientID,username,password):
-        command = 'AT+QMTCONN='+str(self.clientidx)+','+clientID+'[,'+username+'[,'+password+'\r'
-        self.initiate.sendATCommand(command)
+        args = self.clientidx+","+clientID+"[,"+username+"[,"+password+"\r\n"
+        Response = self.sendATCommand(CONNECT_MQTT,args)
+        return Response
 
     def disconnect_mqtt(self,clientID,username,password):
-        command = 'AT+QMTDISC='+str(self.clientidx)+'\r'
-        self.initiate.sendATCommand(command)
+        args = self.clientidx+"\r\n"
+        Response = self.sendATCommand(DISCONNECT_MQTT,args)
+        return Response
 
-    def subscribe_mqtt(self,msgID,topic,qos,):
-        command = 'AT+QMTSUB='+str(self.clientidx)+','+str(msgID)+','+topic+','+str(qos)+'\r'
-        self.initiate.sendATCommand(command)
+    def subscribe_mqtt(self,msgID,topic,qos):
+        args = self.clientidx+","+msgID+","+topic+","+str(qos)+"\r\n"
+        Response = self.sendATCommand(SUBSCRIBE_MQTT,args)
+        return Response
 
     def unsebscribe_mqtt(self,msgID,topic):
-        command = 'AT+QMTUNS='+str(self.clientidx)+','+str(msgID)+','+topic+','+'\r'
-        self.initiate.sendATCommand(command)
-
+        args = self.clientidx+","+msgID+","+topic+","+"\r\n"
+        Response = self.sendATCommand(UNSUBSCRIBE_MQTT,args)
+        return Response
     def publish_mqtt(self,msgID,qos,retain,topic,msg_len):
-        command = 'AT+QMTPUBX='+str(self.clientidx)+','+str(msgID)+','+str(qos)+','+str(retain)+','+topic+','+str(msg_len)+'\r'
-        self.initiate.sendATCommand(command)
+        args= str(self.clientidx)+','+str(msgID)+','+str(qos)+','+str(retain)+','+topic+','+str(msg_len)+'\r\n'
+        Response = self.sendATCommand(PUBLISH_MQTT,args)
+        return Response
 
-    def readMessage_mqtt(self):
-        command = 'AT+QMTRECV='+str(self.clientidx)+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
+    def readMessage_mqtt(self, args=clientidx):
+        Response=self.sendATCommand(READ_MSG_MQTT,args)
+        return Response
 
 
 
