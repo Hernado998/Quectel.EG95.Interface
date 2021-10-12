6c107c4f0b6fcae5e739ad22ea312904b01e8c22
Hermassi Nadir <69248484+Hernado998@users.noreply.github.com>
2021-10-12 22:41:25 +0100

    Update homefunc.py

--- homefunc.py
+++ homefunc.py
@@ -1,46 +1,113 @@
-import serial 
+"""
+    eg9x class definition
+    EG9X class to control and manage the EG9x module 
+    telecommunications Engineering Students @ SUPCOM [ HIGHER SCHOOL OF COMMUNICATIONS OF TUNIS]
+    date: 12/10/2021
+"""
+from EG9x.atcommand import *
+import serial.tools.list_ports
 import time
-from InitiateCom import InitiateCom
 
 
-serialPort = "/dev/ttyUSB0"
-class QuectelEG95Home:
+class eg9x:
+
+    ''' 
+    Connect the module through UART
+    '''
     def __init__(self):
-        self.initiate=InitiateCom(serialPort)
-        self.initiate.connectQuectelEG95()
-    def requestManufacturerId(self):
-        command = 'AT+CGMI'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-    def deviceModule(self):
-        command = 'AT+CGMM'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-    def firmwareVersion(self):
-        command = 'AT+GMM'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-    def networkRegistration(self):
-        command = 'AT+CREG?'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-    def grpsNetworkStatus(self,attachmode):
-        command = 'AT+QCFG="gprsattach"[,'+str(attachmode)+']'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-    def preferredOperator(self):
-        command = 'AT+CPOL'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-    def simCardIMSI(self):
-        command = 'AT+CIMI'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-    def simCardICCID(self):
-        command = 'AT+QCCID'+'\r'
-        self.initiate.sendATCommand(command)
-        return self.initiate.readData()
-
-
-
+        ports = serial.tools.list_ports.comports()
+        for port in ports:
+            ser=serial.Serial(port, 460800, timeout=5)
+            if(ser.is_open):
+                self.SerialPort=port
+                break    
+        self.serial=serial.Serial(self.SerialPort, 460800, timeout=0.3)
         
+
+    ''' 
+    Check for the response from the Module
+    @data : the returned data from the module
+    '''
+    def checkResponse(self,data):
+        Status=""
+        if(len(data)==0):
+            Status="ERROR"
+            return Status
+        for character in range(len(data)):
+            start=time.time()
+            if data[character]=="O" and data[character+1]=="K":
+                Status=data
+                break
+
+        return Status
+
+    ''' 
+    Send At Commands to the Module
+    @command : the AT Command to Send
+    @args    : Arguments if needed for the At Command
+    '''
+    def sendATCommand(self,command,args=None):
+        command=AT_COMMANDS(command)
+        to_send=""                          # Command to send
+        if (len(command.get_string)!=0):
+            to_send+=command.get_string()
+        if(len(args)!=0):
+            to_send+=args
+        ser=self.serial                     
+        ser.write(to_send)                  # Send the Command
+        results=ser.readlines()
+        status=self.checkResponse(results)
+        if(status):
+            return results
+        return status
+    '''
+    Get Manufacturer ID of the module
+    '''
+    def requestManufacturerId(self):
+        Response= self.sendATCommand(MANIFACTURE_ID)
+        return Response
+    '''
+    Get Device ID of the module
+    '''
+    def deviceModule(self):
+        Response= self.sendATCommand(DEVICE_ID)
+        return Response
+    '''
+    Get firmeware of the module
+    '''
+    def firmwareVersion(self):
+        Response=self.sendATCommand(FIRMWARE_VS)
+        return Response
+    
+    '''
+    Get Network Registration 
+    '''   
+    def networkRegistration(self,args="=?"):
+        Response=self.sendATCommand(REGISTRED_DEVICE_STATUS,args)
+        return Response
+    '''
+    Get GPRS Status 
+    @args : the attached mode 
+    '''       
+    def grpsNetworkStatus(self,args="=gprsattach"):
+        Response=self.sendATCommand(EXTENDED_CONF_SET,args)
+        return Response
+    '''
+    Get list of Preferred Operators
+    @args : test / read / write , default to Read
+    '''    
+    def preferredOperator(self,args="=?"):
+        Response=self.sendATCommand(PREFERED_OPERATORS_LIST,args)
+        return Response
+    '''
+    Get Request International Mobile Subscriber Identity (IMSI)
+    '''     
+    def simCardIMSI(self):
+        Response=self.sendATCommand(SIM_CARD_IMSI)
+        return Response
+    '''
+    Get Integrated Circuit Card Identifier
+    '''         
+    def simCardICCID(self):
+        Response=self.sendATCommand(SIM_CARD_ICCID)
+        return Response
