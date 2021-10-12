19ecb12398c2471001fa7a9aa85e8502687e7861
Hermassi Nadir <69248484+Hernado998@users.noreply.github.com>
2021-10-12 23:35:13 +0100

    Update mqttfunc.py

--- mqttfunc.py
+++ mqttfunc.py
@@ -1,6 +1,9 @@
 from paho.mqtt import client as mqtt_client
 from atcommand import *
 
+'''
+Parameters for testing purposes
+'''
 broker = "10.42.0.219"
 port = "1883"
 topic = "test"
@@ -12,45 +15,79 @@ password = "admin"
 
 class QuectelEG95Mqtt:
 
+    '''
+    Initialize the brokers parameters and the client idx
+    '''
     def __init__(self,clientidx,broker,port):
         self.clientidx = clientidx
         self.broker = broker
         self.port=port
-
+        
+    '''
+    Open a MQTT network to enable client-broker connexion
+    '''
     def openNetwork_mqtt(self):
         args= self.clientidx+","+self.broker+","+self.port+"\r\n"
         Response = self.sendATCommand(OPEN_NETWORK_MQTT,args)
         return Response
     
+    '''
+    Close MQTT network
+    '''
     def closeNetwork_mqtt(self):
         args = self.clientidx+"\r\n"
         Response = self.sendATCommand(CLOSE_NETWORK_MQTT,args)
         return Response
     
+    '''
+    Connect to MQTT broker
+    '''
     def connect_mqtt(self,clientID,username,password):
         args = self.clientidx+","+clientID+"[,"+username+"[,"+password+"\r\n"
         Response = self.sendATCommand(CONNECT_MQTT,args)
         return Response
-
+    
+    '''
+    Disconnect from the MQTT broker
+    '''
     def disconnect_mqtt(self,clientID,username,password):
         args = self.clientidx+"\r\n"
         Response = self.sendATCommand(DISCONNECT_MQTT,args)
         return Response
 
+    '''
+    Subscribe to a specific topic and a QoS 0 or 1 or 2
+    0: fire and forget
+    1: send multiple pakages until one of them arrives 
+    2: send one package at a time until we get an acknowledgment
+    '''
     def subscribe_mqtt(self,msgID,topic,qos):
         args = self.clientidx+","+msgID+","+topic+","+str(qos)+"\r\n"
         Response = self.sendATCommand(SUBSCRIBE_MQTT,args)
         return Response
-
+    
+    '''
+    Unsubscribe
+    '''
     def unsebscribe_mqtt(self,msgID,topic):
         args = self.clientidx+","+msgID+","+topic+","+"\r\n"
         Response = self.sendATCommand(UNSUBSCRIBE_MQTT,args)
         return Response
+    
+    '''
+    Publish a message under a topic with a specific QoS
+    0: fire and forget
+    1: send multiple pakages until one of them arrives 
+    2: send one package at a time until we get an acknowledgment
+    '''
     def publish_mqtt(self,msgID,qos,retain,topic,msg_len):
         args= str(self.clientidx)+','+str(msgID)+','+str(qos)+','+str(retain)+','+topic+','+str(msg_len)+'\r\n'
         Response = self.sendATCommand(PUBLISH_MQTT,args)
         return Response
-
+    
+    '''
+    Read message comming from the broker
+    '''
     def readMessage_mqtt(self):
         args=self.clientidx
         Response=self.sendATCommand(READ_MSG_MQTT,args)
