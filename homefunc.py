4f2caa9060f26acc0f873565bc1dddc251505cd8
S0UL4 <62818840+S0UL4@users.noreply.github.com>
2021-10-17 16:07:50 +0100

    update EG9x Quectel's Library

--- homefunc.py
+++ homefunc.py
@@ -1,50 +1,53 @@
 """
-    eg9x class definition
-    EG9X class to control and manage the EG9x module 
+    eg9x class definition to control and manage the EG9x module 
+    Authors : Iheb Omar Soula and Nadir Hermassi
     telecommunications Engineering Students @ SUPCOM [ HIGHER SCHOOL OF COMMUNICATIONS OF TUNIS]
+    Project in Sup'Com with Comelit R&D company
     date: 12/10/2021
 """
+#from EG9x.atcommand import *
 from atcommand import *
 import serial.tools.list_ports
 import time
 
 
-class eg9x:
+class QuectelEG9x:
 
     ''' 
     Connect the module through UART
     '''
     def __init__(self):
         ports = serial.tools.list_ports.comports()
-        for port in ports:
-            ser=serial.Serial(port, 460800, timeout=5)
+        if(len(ports)==0):
+            print("Please Connect the module to USB Cable\n")
+            while(len(ports)==0):
+                ports = serial.tools.list_ports.comports()
+        for port,desc, hwid in ports:
+            ser=serial.Serial(port, 115200, timeout=5)
             if(ser.is_open):
                 self.SerialPort=port
                 break    
-        self.serial=serial.Serial(self.SerialPort, 460800, timeout=0.3)
+        self.serial=serial.Serial(self.SerialPort, 115200, timeout=5)
+        print("Instanciation of Object : Done")
         
-    '''
-    Disconnect the module
-    '''
-    def disconnectQuectelEG95(self):
-        self.serial.close()
-        
-        
+
     ''' 
     Check for the response from the Module
     @data : the returned data from the module
     '''
     def checkResponse(self,data):
-        Status=""
+        Status=bytes("", 'utf-8')
         if(len(data)==0):
-            Status="ERROR"
+            Status=bytes("ERROR", 'utf-8')
             return Status
-        for character in range(len(data)):
-            start=time.time()
-            if data[character]=="O" and data[character+1]=="K":
-                Status=data
-                break
-
+        
+        for string in data:
+            for character in range(len(str(string))-1):
+                ch=str(string)
+                if ch[character]=="O" and ch[character+1]=="K":
+                    for byte in data:
+                        Status+=byte
+                    break
         return Status
 
     ''' 
@@ -52,20 +55,18 @@ class eg9x:
     @command : the AT Command to Send
     @args    : Arguments if needed for the At Command
     '''
-    def sendATCommand(self,command,args=None):
-        command=AT_COMMANDS(command)
+    def sendATCommand(self,command,args=""):
         to_send=""                          # Command to send
-        if (len(command.get_string)!=0):
-            to_send+=command.get_string()
+        if (len(command.string)!=0):
+            to_send+=command.string
         if(len(args)!=0):
-            to_send+=args
-        ser=self.serial                     
-        ser.write(to_send)                  # Send the Command
-        results=ser.readlines()
+            to_send=to_send[:-2]+args+str('\r\n')
+        ser=self.serial  
+        time.sleep(2)             
+        ser.write(bytes(to_send, 'utf-8'))                  # Send the Command
+        results=ser.readlines()  
         status=self.checkResponse(results)
-        if(status):
-            return results
-        return status
+        return status.decode("utf-8")
     '''
     Get Manufacturer ID of the module
     '''
@@ -88,11 +89,12 @@ class eg9x:
     '''
     Get Network Registration 
     '''   
-    def networkRegistration(self,args="=?"):
+    def networkRegistration(self,args="?"):
         Response=self.sendATCommand(REGISTRED_DEVICE_STATUS,args)
         return Response
     '''
-    Get GPRS Status 
+    Get Extended functions
+    default to get GPRS Status 
     @args : the attached mode 
     '''       
     def grpsNetworkStatus(self,args="=gprsattach"):
@@ -102,7 +104,7 @@ class eg9x:
     Get list of Preferred Operators
     @args : test / read / write , default to Read
     '''    
-    def preferredOperator(self,args="=?"):
+    def preferredOperator(self,args="?"):
         Response=self.sendATCommand(PREFERED_OPERATORS_LIST,args)
         return Response
     '''
@@ -117,3 +119,88 @@ class eg9x:
     def simCardICCID(self):
         Response=self.sendATCommand(SIM_CARD_ICCID)
         return Response
+    '''
+    Get SMS mode Status
+    Default to TXT
+    '''       
+    def GetSMSMode(self):
+        Response=self.sendATCommand(GET_SMS_MODE)
+        return Response  
+    '''
+    Set SMS mode Status
+    Default to TXT 
+    @args : the attached mode 
+    '''       
+    def SetSMSMode(self,args="1"):
+        Response=self.sendATCommand(SET_SMS_MODE,args)
+        return Response         
+    '''
+    Set SMS TE default Char
+    GSM 7 bit default alphabet
+    @args : the attached mode 
+    '''       
+    def SetSMSChar(self,args="GSM"):
+        Response=self.sendATCommand(SET_SMS_CHAR,args)
+        return Response  
+
+
+    '''
+    Send SMS Message
+    @args : the attached mode 
+    '''       
+    def SendSMS(self):
+        mode=1
+        if "PDU" in str(self.GetSMSMode()):
+            mode=0
+        if(mode):
+            number=input("Enter Number\n")
+            self.sendATCommand(SEND_SMS_NUMBER,number)
+            message_text=input("Enter Message\n")
+            Response=self.sendATCommand(SEND_SMS_NUMBER,message_text)
+            return Response
+        else:
+            length=input("Enter Length\n")
+            self.sendATCommand(SEND_SMS_NUMBER,length)
+            try:
+                message_Hex=input("Enter Hex Message\n")
+                is_hex=int(message_Hex, 16)
+                Response=self.sendATCommand(SEND_SMS_NUMBER,message_Hex)
+                return Response
+            except:
+                print("Message should be HEX\n")
+                return "ERROR\n"
+            
+    '''
+    Get SMS Message Storage Area
+    '''
+    def GET_SMS_STORAGE_AREA(self):
+        Response=self.sendATCommand(GET_SMS_STORAGE_AREA)
+        return  Response
+    '''
+    Set SMS Message Storage Area
+    @args : the attached Storage Area
+    Default to SM : SIM CARD STORAGE 
+    '''
+    def SET_SMS_STORAGE_AREA(self,args="“SM”,“SM”,“SM”"):
+        Response=self.sendATCommand(SET_SMS_STORAGE_AREA,args)
+        return Response
+    '''
+    Read SMS Message LIST
+    @args : the attached Storage Area
+    Default : list ALL message in text mode 
+    '''
+    def READ_LIST_MESSAGE(self,args="ALL"):
+        mode=self.GetSMSMode()
+        print(mode)
+        if("PDU" in str(mode)):
+            mode=0
+        else:
+            mode=1
+        if((args in ["ALL","REC UNREAD","REC READ","STO UNSENT","STO SENT"]) and mode): #READ MESSAGE TEXT
+            Response=self.sendATCommand(READ_SMS_LIST,args)
+        elif((args in ["0","1","2","3","4"]) and not(mode)):                              #READ MESSAGE PDU
+            Response=self.sendATCommand(READ_SMS_LIST,args)
+        else:
+            Response="+CMS ERROR: <err>"
+        return Response
+        
